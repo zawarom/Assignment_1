e36f865092d97b830c09a0060000e461ed60c139
om <zawarom15@gmail.com>
2023-11-29 23:26:51 +0530

    final commit

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>From app</title>
+    <title>MS Dhoni</title>
 </head>
 <body>
     @include('layout.navbar')
+    
     @yield('contents')
+
+    @include('layout.footer')
 </body>
 </html>
